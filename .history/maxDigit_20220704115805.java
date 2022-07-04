9e26d7d9b95a133a918a70f304f2a1b84582e4be
Goutam Raj <74093499+GoUtAmRaj07@users.noreply.github.com>
2022-07-04 20:32:20 +0530

    New update

---
+++ .history/maxDigit_20220704115805.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class maxDigit {
+    private static int maxdigit(String s) {
+        int maxDigit = 0;
+        for (int i = 0; i < s.length(); i++) {
+            System.out.println("current digit in the string : " + Integer.parseInt(s, s.charAt(i)));
+            maxDigit = Math.max(maxDigit, Integer.valueOf(s.charAt(i)));
+            System.out.println("max digit in the string : " + maxDigit);
+        }
+        return maxDigit;
+    }
+
+    public static void main(String[] args) {
+        String s = "34739";
+
+        int n = maxdigit(s);
+        System.out.println(n);
+    }
+}
---
+++ maxDigit.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class maxDigit {
+    private static int maxdigit(String s) {
+        int maxDigit = 0;
+        for (int i = 0; i < s.length(); i++) {
+            System.out.println("current digit in the string : " + Integer.valueOf(s.charAt(i) - '0'));
+            maxDigit = Math.max(maxDigit, Integer.valueOf(s.charAt(i) - '0'));
+            System.out.println("max digit in the string : " + maxDigit);
+        }
+        return maxDigit;
+    }
+
+    public static void main(String[] args) {
+        String s = "34739";
+
+        int n = maxdigit(s);
+        System.out.println(n);
+    }
+}
